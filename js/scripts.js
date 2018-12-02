162e0b2fceebeb67a758c450fdf62af8352d981b
Marcin <marcingorecki1981@gmail.com>
2018-12-02 23:26:54 +0100

    Add files via upload

--- js/scripts.js
+++ js/scripts.js
@@ -1,74 +1,109 @@
-fetch('js/pytania.json').then(response => {
+fetch("js/pytania.json")
+  .then(response => {
     return response.json();
-}).then(pytania => {  
+  })
+  .then(pytania => {
     init(pytania);
-});
+  });
 
 function init(pytania) {
+  const main = document.querySelector("main");
 
-    const main = document.querySelector("main");
+  const hide = los => {
+    document.querySelector("#media").classList.remove("media_visible");
+    document.querySelector("#media").innerHTML = "";
+    document.querySelector("#media").style.background = "";
+    pytania[los].typ === "img" || pytania[los].typ === "yt"
+      ? (main.style.paddingTop = "0")
+      : (main.style.paddingTop = "20vh");
+  };
 
-    const hide = (los) => {
-        document.querySelector("#media").classList.remove("media_visible");
-        document.querySelector("#media").innerHTML = "";
-        document.querySelector("#media").style.background = "";
-        pytania[los].typ === "img" || pytania[los].typ === "yt" ? main.style.paddingTop = "0" : main.style.paddingTop = "20vh";
-    }
+  const zwyklePytania = los => {
+    hide(los);
+    document.querySelector("#pytanie").textContent = pytania[los].tekst;
+    document.querySelector("#numer").textContent = pytania[los].numer + 1;
+    document.querySelector("#epoka").textContent = pytania[los].epoka;
+  };
 
-    const zwyklePytania = (los) => {
-        hide(los);
-        document.querySelector("#pytanie").textContent = pytania[los].tekst;
-        document.querySelector("#numer").textContent = pytania[los].numer + 1;
-        document.querySelector("#epoka").textContent = pytania[los].epoka;
-    }
+  const media = los => {
+    hide(los);
+    document.querySelector("#numer").textContent = pytania[los].numer + 1;
+    document.querySelector("#epoka").textContent = pytania[los].epoka;
+    document.querySelector("#pytanie").textContent = pytania[los].tekst;
+    document.querySelector("#media").classList.add("media_visible");
+    document.querySelector("#media").style.backgroundImage = `url(images/${
+      pytania[los].image
+    })`;
+  };
 
-    const media = (los) => {
-        hide(los);
-        document.querySelector("#numer").textContent = pytania[los].numer + 1;
-        document.querySelector("#epoka").textContent = pytania[los].epoka;
-        document.querySelector("#pytanie").textContent = pytania[los].tekst;
-        document.querySelector("#media").classList.add("media_visible");
-        document.querySelector("#media").style.background = `url(images/${pytania[los].image})`;
-    }
+  const yt = los => {
+    hide(los);
+    document.querySelector("#numer").textContent = pytania[los].numer + 1;
+    document.querySelector("#epoka").textContent = pytania[los].epoka;
+    document.querySelector("#pytanie").textContent = pytania[los].tekst;
+    document.querySelector("#media").classList.add("media_visible");
+    let ifrm = document.createElement("iframe");
+    ifrm.setAttribute("width", pytania[los].width);
+    ifrm.setAttribute("height", pytania[los].height);
+    ifrm.setAttribute("src", pytania[los].src);
+    ifrm.setAttribute("modestbranding", "0");
+    document.querySelector("#media").appendChild(ifrm);
+  };
 
-    const yt = (los) => {
-        hide(los);
-        document.querySelector("#numer").textContent = pytania[los].numer + 1;
-        document.querySelector("#epoka").textContent = pytania[los].epoka;
-        document.querySelector("#pytanie").textContent = pytania[los].tekst;
-        document.querySelector("#media").classList.add("media_visible");
-        let ifrm = document.createElement('iframe');
-        ifrm.setAttribute("width", pytania[los].width);
-        ifrm.setAttribute("height", pytania[los].height);
-        ifrm.setAttribute("src", pytania[los].src);
-        ifrm.setAttribute("modestbranding", "0");
-        document.querySelector("#media").appendChild(ifrm);
-    }
-    
-    function losujPytanie(min, max) {
-        return Math.random() * (max - min) + min;
-    }
+  function losujPytanie(min, max) {
+    return (Math.random() * (max - min) + min).toFixed(0);
+  }
 
-    function sygnalizujKoniec() {
-        alert("Nie ma więcej pytań!");
-    }
+  function sygnalizujKoniec() {
+    alert("Nie ma więcej pytań!");
+  }
 
-    document.addEventListener("keyup", function(e) {
-        if(e.keyCode === 32) {
-            let ilosc = pytania.length-1;
-            if(ilosc < 0) {
-                sygnalizujKoniec();
-                return;
-            }
-            let los = losujPytanie(0, ilosc).toFixed(0);
-            if(pytania[los].typ === "reg") {
-                zwyklePytania(los);   
-            } else if(pytania[los].typ === "yt") {
-                yt(los);
-            } else {
-                media(los);
-            }
-            pytania.splice(los, 1);
+  const pokazKarte = los => {
+    let karta = document.querySelector("#losowanie");
+    let nr_na_karcie = document.querySelector("#losowanie span");
+    let epoka = document.querySelector("#losowanie p");
+    epoka.textContent = "";
+    let i = 1;
+    karta.style.display = "flex";
+    let interval = setInterval(function() {
+      nr_na_karcie.textContent = (Math.random() * (84 - 1) + 1).toFixed(0);
+      i++;
+      if (i > 30) {
+        clearInterval(interval);
+        nr_na_karcie.textContent = `${pytania[los].numer + 1}`;
+        epoka.textContent = `${pytania[los].epoka}`;
+      }
+    }, 100);
+  };
+
+  document.addEventListener("keyup", function(e) {
+    if (e.keyCode === 32) {
+      let ilosc = pytania.length - 1;
+      if (ilosc < 0) {
+        sygnalizujKoniec();
+        return;
+      }
+      let los = losujPytanie(0, ilosc);
+      pokazKarte(los);
+      setTimeout(function() {
+        if (pytania[los].typ === "reg") {
+          zwyklePytania(los);
+        } else if (pytania[los].typ === "yt") {
+          yt(los);
+        } else {
+          media(los);
         }
-    })
+        document.querySelector("#losowanie").style.display = "none";
+        pytania.splice(los, 1);
+      }, 5000);
+      //   if (pytania[los].typ === "reg") {
+      //     zwyklePytania(los);
+      //   } else if (pytania[los].typ === "yt") {
+      //     yt(los);
+      //   } else {
+      //     media(los);
+      //   }
+      //   pytania.splice(los, 1);
+    }
+  });
 }
